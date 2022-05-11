5245cc8a4005fd02f5875dee0d1d5f72a5f04e44
HopeZucco <hmz34@drexel.edu>
2022-05-10 21:21:39 -0400

    svg and plugins import

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,16 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+
+gsap.registerPlugin(MorphSVGPlugin, DrawSVGPlugin);
+
+const maintTL = gsap.timeline();
+  
+maintTL.from(MorphSVGPlugin.convertToPath("swirl"))
+.from("swirl", {duration:1, drawSVG:"50% 50%"})
+.from("swirl", {duration:1, fill:"brown"}, "-=75%")
+.to("swirl", {duration:1, fill:"fff"})
+
+
+
+;
